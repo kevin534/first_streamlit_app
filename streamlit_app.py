2da608a6b90083281ff64e2b21e53f6864c070dc
KEVIN XAVIER TOUKAM <60352713+kevin534@users.noreply.github.com>
2023-05-22 13:26:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 e Blueberry Oatmeal')
+
+streamlit.text('Kale, Spinach e Rocket Smoothie')
+
+streamlit.text('Hard-Boiled Free-Range Egg')
